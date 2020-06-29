151a70649e6d85bc567a09c7f46bcfd6bec12a92
孙伟 <me@sunwei.xyz>
2020-06-29 21:48:42 +0800

    Initial commit

---
+++ src/main/java/xyz/sunwei/batch/controller/AppController.java
@@ -0,0 +1,12 @@
+package xyz.sunwei.batch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class AppController {
+    @GetMapping
+    public String helloChange() {
+        return "Hello Web MVC!";
+    }
+}
